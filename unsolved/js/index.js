29b40c82d19fdfae468fca897662103f09d221bf
njsof05 <141085909+njsof05@users.noreply.github.com>
2024-01-16 21:39:29 -0600

    Initial commit

---
+++ unsolved/js/index.js
@@ -0,0 +1,17 @@
+// Create event listener to enter items below
+
+// Create event listener to delete items below
+
+// Create event listener to toggle 
+$("enter").on("click", function(){var task=$(todoItem).val()
+$(todoList).append("<div class='task'>"+task+"<div class='x fas fa-times'></div></div>");});
+
+$(document).on("click", ".x", function(){$(this).parent().remove();});
+
+$(document),on("click", ".task", function(){$(this).toggleClass("done")
+if ($(this).hasClass("done")) {$(this).find("div").removeClass("fa-times");
+$(this).find("div").addClass("fa-check");}
+
+else {$(this).find("div").addClass("fa-times");
+
+}});
